e04f096008f8267cecac349429f209d15b4744af
yangxin <yx_yoyo@live.cn>
2017-03-16 18:43:21 +0800

    修改npm install shell

--- post-install.sh
+++ post-install.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-rm -rf node_modules/appium-base-driver
-rm -rf node_modules/appium-android-driver
-rm -rf node_modules/appium-gulp-plugins
-rm -rf node_modules/appium-ios-driver
-
-mv appium-base-driver node_modules/
-mv appium-android-driver node_modules/
-mv appium-gulp-plugins node_modules/
-mv appium-ios-driver node_modules/
+mv node_modules/appium-base-driver/git node_modules/appium-base-driver/.git
+mv node_modules/appium-android-driver/git node_modules/appium-android-driver/.git
+mv node_modules/appium-gulp-plugins/git node_modules/appium-gulp-plugins/.git
+mv node_modules/appium-ios-driver/git node_modules/appium-ios-driver/.git
--- pre-install.sh
+++ pre-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mv node_modules/appium-base-driver ./
-mv node_modules/appium-android-driver/ ./
-mv node_modules/appium-gulp-plugins/ ./
-mv node_modules/appium-ios-driver ./
+mv node_modules/appium-base-driver/.git node_modules/appium-base-driver/git
+mv node_modules/appium-android-driver/.git node_modules/appium-android-driver/git
+mv node_modules/appium-gulp-plugins/.git node_modules/appium-gulp-plugins/git
+mv node_modules/appium-ios-driver/.git node_modules/appium-ios-driver/git
