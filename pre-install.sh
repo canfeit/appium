73022114da13f6a3f29446964187142aeb066f13
yangxin <yx_yoyo@live.cn>
2017-03-18 20:34:20 +0800

    安装脚本变更

--- pre-install.sh
+++ pre-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mv node_modules/appium-adb/.git node_modules/appium-adb/git
 mv node_modules/appium-base-driver/.git node_modules/appium-base-driver/git
 mv node_modules/appium-android-driver/.git node_modules/appium-android-driver/git
 mv node_modules/appium-gulp-plugins/.git node_modules/appium-gulp-plugins/git
