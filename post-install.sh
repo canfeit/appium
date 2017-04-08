c3fee0c0683f8fe95e8cec2ca468e2534653e444
yangxin <yx_yoyo@live.cn>
2017-04-08 16:25:41 +0800

    增加xcuitest driver 安装脚本

--- post-install.sh
+++ post-install.sh
@@ -5,3 +5,4 @@ mv node_modules/appium-base-driver/git node_modules/appium-base-driver/.git
 mv node_modules/appium-android-driver/git node_modules/appium-android-driver/.git
 mv node_modules/appium-gulp-plugins/git node_modules/appium-gulp-plugins/.git
 mv node_modules/appium-ios-driver/git node_modules/appium-ios-driver/.git
+mv node_modules/appium-xcuitest-driver/git node_modules/appium-xcuitest-driver/.git
